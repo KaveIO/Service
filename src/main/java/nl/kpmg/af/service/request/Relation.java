6258e664adb77710329c3ff221e0401e792c72cf
Hoekstra.Maarten <Hoekstra.Maarten@GL6HZ3J.kmsgroep.com>
2013-09-04 16:41:48 +0200

    Implemented functionality for querying events based on their node or edge id

--- src/main/java/nl/kpmg/af/service/request/Relation.java
+++ src/main/java/nl/kpmg/af/service/request/Relation.java
@@ -1,19 +1,55 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package nl.kpmg.af.service.request;
 
+import com.mongodb.BasicDBObject;
 import com.mongodb.DBObject;
-import java.util.Map;
+import java.util.List;
 import nl.kpmg.af.datamodel.dao.exception.DataModelException;
+import org.bson.types.ObjectId;
 
 /**
  *
  * @author Hoekstra.Maarten
  */
 public class Relation {
+    private Type type;
+    private List<ObjectId> ids;
+
+    public enum Type {
+        NODE("nodeId"),
+        EDGE("edgeId");
+        private final String fieldName;
+
+        private Type(String fieldName) {
+            this.fieldName = fieldName;
+        }
+
+        public String getFieldName() {
+            return fieldName;
+        }
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public List<ObjectId> getIds() {
+        return ids;
+    }
+
+    public void setIds(List<ObjectId> ids) {
+        this.ids = ids;
+    }
+
     public DBObject getMongoCondition() throws DataModelException {
-        return null;
+        if (type != null && ids.size() > 0) {
+            DBObject query = new BasicDBObject(type.getFieldName(), new BasicDBObject("$in", ids));
+            return query;
+        } else {
+            throw new DataModelException("Malformed request");
+        }
     }
 }
