253a58a2e9b87ddd70218d6b9d7585601340b9c6
Hoekstra.Maarten <Hoekstra.Maarten@GL6HZ3J.kmsgroep.com>
2013-09-06 14:25:23 +0200

    Fixed Checkstyle compliance for EdgeDto

--- src/main/java/nl/kpmg/af/service/response/dto/EdgeDto.java
+++ src/main/java/nl/kpmg/af/service/response/dto/EdgeDto.java
@@ -3,71 +3,138 @@ package nl.kpmg.af.service.response.dto;
 import java.util.Map;
 
 /**
+ * Data Transfer Object for Edge.
+ * The auto-serialization of JAX-RS is really convenient but will serialize complex objects (such as ObjectId) in an
+ * undesirable fashion. In order to avoid this problem a couple of DTO's are made. These are used to transfer business
+ * objects to client applications.
  *
  * @author Hoekstra.Maarten
  */
 public class EdgeDto {
+    /**
+     * The unique mongo id of this object.
+     */
     private String id;
+    /**
+     * The type to which this object belongs.
+     */
     private String type;
+    /**
+     * The name of this edge.
+     */
     private String name;
+    /**
+     * The description of this edge.
+     */
     private String description;
+    /**
+     * A map with the specific contents of this edge.
+     */
     private Map meta;
+    /**
+     * The mongo id of the source node to this edge.
+     */
     private String sourceId;
+    /**
+     * The mongo id of the target node to this edge.
+     */
     private String targetId;
 
-    public String getId() {
+    /**
+     * @return The unique mongo id of this object.
+     */
+    public final String getId() {
         return id;
     }
 
-    public void setId(String id) {
+    /**
+     * @param id The unique mongo id of this object.
+     */
+    public final void setId(final String id) {
         this.id = id;
     }
 
-    public String getType() {
+    /**
+     * @return type The type to which this object belongs.
+     */
+    public final String getType() {
         return type;
     }
 
-    public void setType(String type) {
+    /**
+     * @param type The type to which this object belongs.
+     */
+    public final void setType(final String type) {
         this.type = type;
     }
 
-    public String getName() {
+    /**
+     * @return The name of this edge.
+     */
+    public final String getName() {
         return name;
     }
 
-    public void setName(String name) {
+    /**
+     * @param name The name of this edge.
+     */
+    public final void setName(final String name) {
         this.name = name;
     }
 
-    public String getDescription() {
+    /**
+     * @return A map with the specific contents of this edge.
+     */
+    public final String getDescription() {
         return description;
     }
 
-    public void setDescription(String description) {
+    /**
+     * @param description A map with the specific contents of this edge.
+     */
+    public final void setDescription(final String description) {
         this.description = description;
     }
 
-    public Map getMeta() {
+    /**
+     * @return A map with the specific contents of this edge.
+     */
+    public final Map getMeta() {
         return meta;
     }
 
-    public void setMeta(Map meta) {
+    /**
+     * @param meta A map with the specific contents of this edge.
+     */
+    public void setMeta(final Map meta) {
         this.meta = meta;
     }
 
-    public String getSourceId() {
+    /**
+     * @return The mongo id of the source node to this edge.
+     */
+    public final String getSourceId() {
         return sourceId;
     }
 
-    public void setSourceId(String sourceId) {
+    /**
+     * @param sourceId The mongo id of the source node to this edge.
+     */
+    public final void setSourceId(final String sourceId) {
         this.sourceId = sourceId;
     }
 
-    public String getTargetId() {
+    /**
+     * @return The mongo id of the target node to this edge.
+     */
+    public final String getTargetId() {
         return targetId;
     }
 
-    public void setTargetId(String targetId) {
+    /**
+     * @param targetId The mongo id of the target node to this edge.
+     */
+    public final void setTargetId(final String targetId) {
         this.targetId = targetId;
     }
 }
