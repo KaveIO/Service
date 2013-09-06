ed8e6d885ff1d37a1b434be781eb23511b34aa34
Hoekstra.Maarten <Hoekstra.Maarten@GL6HZ3J.kmsgroep.com>
2013-09-06 14:19:11 +0200

    Fixed Checkstyle compliance for EventDto

--- src/main/java/nl/kpmg/af/service/response/dto/EventDto.java
+++ src/main/java/nl/kpmg/af/service/response/dto/EventDto.java
@@ -4,80 +4,156 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
+ * Data Transfer Object for Event.
+ * The auto-serialization of JAX-RS is really convenient but will serialize complex objects (such as ObjectId) in an
+ * undesirable fashion. In order to avoid this problem a couple of DTO's are made. These are used to transfer business
+ * objects to client applications.
  *
  * @author Hoekstra.Maarten
  */
 public class EventDto {
+    /**
+     * The unique mongo id of this object.
+     */
     private String id;
+    /**
+     * The layer from which this object was fetched.
+     */
     private String layer;
+    /**
+     * The mongo id of the related node.
+     */
     private String nodeId;
+    /**
+     * The mongo id of the related edge.
+     */
     private String edgeId;
+    /**
+     * The location expressed in GeoJSON where this event took place.
+     */
     private GeoJSONPositionDto location;
+    /**
+     * The urgency of this event.
+     */
     private String priority;
+    /**
+     * The timestamp expressed in seconds from epoch on which this event took place.
+     */
     private Integer timestamp;
+    /**
+     * A map with the specific contents of this event.
+     */
     private Map value = new HashMap();
 
-    public String getId() {
+    /**
+     * @return The unique mongo id of this object
+     */
+    public final String getId() {
         return id;
     }
 
-    public void setId(String id) {
+    /**
+     * @param id The unique mongo id of this object
+     */
+    public final void setId(final String id) {
         this.id = id;
     }
 
-    public String getLayer() {
+    /**
+     * @return The layer from which this object was fetched.
+     */
+    public final String getLayer() {
         return layer;
     }
 
-    public void setLayer(String layer) {
+    /**
+     * @param layer The layer from which this object was fetched.
+     */
+    public final void setLayer(final String layer) {
         this.layer = layer;
     }
 
-    public String getNodeId() {
+    /**
+     * @return The mongo id of the related node.
+     */
+    public final String getNodeId() {
         return nodeId;
     }
 
-    public void setNodeId(String nodeId) {
+    /**
+     * @param nodeId The mongo id of the related node.
+     */
+    public final void setNodeId(final String nodeId) {
         this.nodeId = nodeId;
     }
 
-    public String getEdgeId() {
+    /**
+     * @return The mongo id of the related edge.
+     */
+    public final String getEdgeId() {
         return edgeId;
     }
 
-    public void setEdgeId(String edgeId) {
+    /**
+     * @param edgeId The mongo id of the related edge.
+     */
+    public final void setEdgeId(final String edgeId) {
         this.edgeId = edgeId;
     }
 
-    public GeoJSONPositionDto getLocation() {
+    /**
+     * @return The location expressed in GeoJSON where this event took place.
+     */
+    public final GeoJSONPositionDto getLocation() {
         return location;
     }
 
-    public void setLocation(GeoJSONPositionDto location) {
+    /**
+     * @param location The location expressed in GeoJSON where this event took place.
+     */
+    public final void setLocation(final GeoJSONPositionDto location) {
         this.location = location;
     }
 
-    public String getPriority() {
+    /**
+     * @return The urgency of this event.
+     */
+    public final String getPriority() {
         return priority;
     }
 
-    public void setPriority(String priority) {
+    /**
+     * @param priority The urgency of this event.
+     */
+    public final void setPriority(final String priority) {
         this.priority = priority;
     }
 
-    public Integer getTimestamp() {
+    /**
+     * @return The timestamp expressed in seconds from epoch on which this event took place.
+     */
+    public final Integer getTimestamp() {
         return timestamp;
     }
 
-    public void setTimestamp(Integer timestamp) {
+    /**
+     * @param timestamp The timestamp expressed in seconds from epoch on which this event took place.
+     */
+    public final void setTimestamp(final Integer timestamp) {
         this.timestamp = timestamp;
     }
 
-    public Map getValue() {
+    /**
+     * @return A map with the specific contents of this event.
+     */
+    public final Map getValue() {
         return value;
     }
 
-    public void setValue(Map value) {
+    /**
+     * @param value A map with the specific contents of this event.
+     */
+    public final void setValue(final Map value) {
         this.value = value;
     }
 }
