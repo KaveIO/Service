59f50e4983a9f29febabf971c9a88f13eb751bd2
Hoekstra.Maarten <Hoekstra.Maarten@GL6HZ3J.kmsgroep.com>
2013-09-06 16:43:34 +0200

    Fixed Checkstyle compliance for Exceptions

--- src/main/java/nl/kpmg/af/service/exception/ServiceInitializationException.java
+++ src/main/java/nl/kpmg/af/service/exception/ServiceInitializationException.java
@@ -5,22 +5,78 @@ package nl.kpmg.af.service.exception;
  * @author Hoekstra.Maarten
  */
 public class ServiceInitializationException extends RuntimeException {
+    /**
+     * Constructs a new runtime exception with {@code null} as its
+     * detail message. The cause is not initialized, and may subsequently be
+     * initialized by a call to {@link #initCause}.
+     */
     public ServiceInitializationException() {
     }
 
-    public ServiceInitializationException(String message) {
+    /**
+     * Constructs a new runtime exception with the specified detail message.
+     * The cause is not initialized, and may subsequently be initialized by a
+     * call to {@link #initCause}.
+     *
+     * @param message the detail message. The detail message is saved for
+     * later retrieval by the {@link #getMessage()} method.
+     */
+    public ServiceInitializationException(final String message) {
         super(message);
     }
 
-    public ServiceInitializationException(String message, Throwable cause) {
+    /**
+     * Constructs a new runtime exception with the specified detail message and
+     * cause. <p>Note that the detail message associated with
+     * {@code cause} is <i>not</i> automatically incorporated in
+     * this runtime exception's detail message.
+     *
+     * @param message the detail message (which is saved for later retrieval
+     * by the {@link #getMessage()} method).
+     * @param cause the cause (which is saved for later retrieval by the
+     * {@link #getCause()} method). (A <tt>null</tt> value is
+     * permitted, and indicates that the cause is nonexistent or
+     * unknown.)
+     * @since 1.4
+     */
+    public ServiceInitializationException(final String message, final Throwable cause) {
         super(message, cause);
     }
 
-    public ServiceInitializationException(Throwable cause) {
+    /**
+     * Constructs a new runtime exception with the specified cause and a
+     * detail message of <tt>(cause==null ? null : cause.toString())</tt>
+     * (which typically contains the class and detail message of
+     * <tt>cause</tt>). This constructor is useful for runtime exceptions
+     * that are little more than wrappers for other throwables.
+     *
+     * @param cause the cause (which is saved for later retrieval by the
+     * {@link #getCause()} method). (A <tt>null</tt> value is
+     * permitted, and indicates that the cause is nonexistent or
+     * unknown.)
+     * @since 1.4
+     */
+    public ServiceInitializationException(final Throwable cause) {
         super(cause);
     }
 
-    public ServiceInitializationException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+    /**
+     * Constructs a new runtime exception with the specified detail
+     * message, cause, suppression enabled or disabled, and writable
+     * stack trace enabled or disabled.
+     *
+     * @param message the detail message.
+     * @param cause the cause. (A {@code null} value is permitted,
+     * and indicates that the cause is nonexistent or unknown.)
+     * @param enableSuppression whether or not suppression is enabled
+     * or disabled
+     * @param writableStackTrace whether or not the stack trace should
+     * be writable
+     *
+     * @since 1.7
+     */
+    public ServiceInitializationException(final String message, final Throwable cause, final boolean enableSuppression,
+            final boolean writableStackTrace) {
         super(message, cause, enableSuppression, writableStackTrace);
     }
 }
