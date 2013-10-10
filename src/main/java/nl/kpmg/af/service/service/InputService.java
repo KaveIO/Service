4b893848925a4dc1fd59e8d54bbd27504c34b8f9
Jan Mennis Amoraal <janos4276@gmail.com>
2013-10-10 13:28:55 +0200

    o Some style cleanup

--- src/main/java/nl/kpmg/af/service/service/InputService.java
+++ src/main/java/nl/kpmg/af/service/service/InputService.java
@@ -1,26 +1,24 @@
 package nl.kpmg.af.service.service;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
+
 import nl.kpmg.af.datamodel.dao.InputDao;
 import nl.kpmg.af.datamodel.dao.exception.DataModelException;
 import nl.kpmg.af.datamodel.model.Input;
 import nl.kpmg.af.service.MongoDBUtil;
 import nl.kpmg.af.service.request.InputRequest;
 import nl.kpmg.af.service.response.assembler.InputAssembler;
+
 import org.json.simple.parser.JSONParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- *
  * @author Hoekstra.Maarten
  */
 @Path("input")
@@ -36,12 +34,12 @@ public final class InputService {
      * Default constructor fetches the DAO from MongoDBUtil.
      */
     public InputService() {
-        inputDao = new InputDao(MongoDBUtil.getMongoDatabase());
+        this.inputDao = new InputDao(MongoDBUtil.getMongoDatabase());
     }
 
     /**
      * Get the corresponding json for the "collection" collection.
-     *
+     * 
      * @param collection the collection of events to fetch from
      * @param request the request which determines which events to return.
      * @return a list of events
@@ -53,7 +51,7 @@ public final class InputService {
     public Response post(@PathParam("collection") final String collection, InputRequest inputDto) {
         try {
             Input input = InputAssembler.assemble(collection, inputDto);
-            inputDao.store(input);
+            this.inputDao.store(input);
         } catch (DataModelException ex) {
             LOGGER.error("Error has occured. Data could not be stored.", ex);
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
