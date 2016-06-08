40940b4ddef42b80b657b38c48587806c1b78aa3
Roel Smits <smits.roel@kpmg.nl>
2016-06-08 11:00:01 +0200

    put and delete methods proxy service

--- src/main/java/nl/kpmg/af/service/v1/ProxyService.java
+++ src/main/java/nl/kpmg/af/service/v1/ProxyService.java
@@ -9,6 +9,8 @@ package nl.kpmg.af.service.v1;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Context;
@@ -49,6 +51,22 @@ public class ProxyService {
 
     @POST
     public Response postProxy(
+            @Context final HttpServletRequest request,
+            @PathParam("applicationId") final String applicationId,
+            @PathParam("name") final String name) {
+        return executeProxy(request, applicationId, name);
+    }
+
+    @DELETE
+    public Response deleteProxy(
+            @Context final HttpServletRequest request,
+            @PathParam("applicationId") final String applicationId,
+            @PathParam("name") final String name) {
+        return executeProxy(request, applicationId, name);
+    }
+
+    @PUT
+    public Response putProxy(
             @Context final HttpServletRequest request,
             @PathParam("applicationId") final String applicationId,
             @PathParam("name") final String name) {
