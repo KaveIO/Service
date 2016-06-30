cf4dac136f3e1a683bb06c23b9458f6eb141a7e6
fziliotto <federico.ziliotto.1@studenti.unipd.it>
2016-06-30 13:48:46 +0200

    DataServiceApplication is the class that contains the web application resources

--- src/main/java/nl/kpmg/af/service/server/DataServiceApplication.java
+++ src/main/java/nl/kpmg/af/service/server/DataServiceApplication.java
@@ -1,19 +1,31 @@
 package nl.kpmg.af.service.server;
 
+import org.glassfish.jersey.jackson.JacksonFeature;
+import org.glassfish.jersey.server.ResourceConfig;
+
 import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+
 import nl.kpmg.af.service.security.BasicAuthFilter;
 import nl.kpmg.af.service.security.CorsFilter;
 import nl.kpmg.af.service.security.PermissionCheckerFilter;
-import org.glassfish.jersey.server.ResourceConfig;
-import org.glassfish.jersey.jackson.JacksonFeature;
 
 /**
  * Created by fziliotto on 29-6-16.
  */
-public class DataServiceApplication  extends ResourceConfig {
-    public DataServiceApplication(){
-        packages(true,"nl.kpmg.af.service.v0");
-        packages(true,"nl.kpmg.af.service.v1");
+public class DataServiceApplication extends ResourceConfig {
+
+    public DataServiceApplication() {
+        init();
+    }
+
+    public DataServiceApplication(String name) {
+        this.setApplicationName(name);
+        init();
+    }
+
+    private void init() {
+        packages(true, "nl.kpmg.af.service.v0");
+        packages(true, "nl.kpmg.af.service.v1");
         register(BasicAuthFilter.class);
         register(PermissionCheckerFilter.class);
         register(CorsFilter.class);
