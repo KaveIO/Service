8aff162c7083be4e1c7717c048363889199e9fa8
fziliotto <federico.ziliotto.1@studenti.unipd.it>
2016-06-30 13:48:06 +0200

    updated BasicAuthTest

--- src/test/java/nl/kpmg/af/service/security/BasicAuthTest.java
+++ src/test/java/nl/kpmg/af/service/security/BasicAuthTest.java
@@ -1,23 +1,43 @@
 package nl.kpmg.af.service.security;
 
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
 
 /**
  * Created by fziliotto on 27-6-16.
  */
 public class BasicAuthTest {
-    private String username="admin";
-    private String password="admin";
-    private String basicAuthString = "Basic YWRtaW46YWRtaW4=";
 
-    @Test
-    public void decodeTest() throws Exception {
-        String[] credential = BasicAuth.decode(basicAuthString);
+	@Test
+	public void decodeTest() throws Exception {
+		String[] credential;
 
-        assertEquals(credential[0],username);
-        assertEquals(credential[1], password);
-    }
+		String username = "admin";
+		String password = "admin";
+		String basicAuthString = "Basic YWRtaW46YWRtaW4="; // auth string with admin:admin
+		credential = BasicAuth.decode(basicAuthString);
+		assertEquals(credential[0], username);
+		assertEquals(credential[1], password);
+
+		String nullAuthString = null;
+		credential = BasicAuth.decode(nullAuthString);
+		assertNull(credential);
+
+		String emptyAuthentication = "";
+		credential = BasicAuth.decode(emptyAuthentication);
+		assertNull(credential);
+
+		String onlyUsernameAuth = "Basic YWRtaW4="; // admin in Base64
+		credential = BasicAuth.decode(onlyUsernameAuth);
+		assertEquals(credential[0], username);
+		assertEquals(credential.length,1);
+
+		String onlyPasswordAuth = "OmFkbWlu";
+		credential = BasicAuth.decode(onlyPasswordAuth);
+		assertEquals(credential[1], password);
+		assertEquals(credential[0], "");
+	}
 
 }
